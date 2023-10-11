d75f8136006506ecbbd4631767fa8d5fbcf1a71a
NBGI\s9-ito <s9-ito@bns-g.com>
2023-10-11 22:53:43 +0900

    update logger

--- Assets/Scripts/Utils/RLog.cs
+++ Assets/Scripts/Utils/RLog.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RLog : MonoBehaviour, ILog
 {
+    [SerializeField]
+    private TextMeshProUGUI _text;
+    private string _textCache = "";
+
     public void Log(string text) {
+        _textCache = _textCache + '\n' + text;
+        _text.text = _textCache;
         Debug.Log(text);
     }
 }
