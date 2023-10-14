0c3028e0b96b4dd1eae7d98b4e408a6d3386deef
asRagi <ragi-justaway@hotmail.co.jp>
2023-10-15 08:11:34 +0900

    is_user

---
+++ Assets/Scripts/InputMessage.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class InputMessage: MonoBehaviour
+{
+    public void OnSubmit(string text)
+    {
+        // Post Message
+
+        // Wait ChatGPT Res
+
+        // Refresh
+    }
+
+    private void OnReturnMessage() {
+        // Post Message
+
+        // Refresh
+    
+    }
+}
