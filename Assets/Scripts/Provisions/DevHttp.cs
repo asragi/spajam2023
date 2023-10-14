ff6c62c2fbaadf6938ddf43fc9141595c8a6b0f4
asRagi <ragi-justaway@hotmail.co.jp>
2023-10-14 11:52:08 +0900

    implemented http function

---
+++ Assets/Scripts/Provisions/DevHttp.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DevHttp : MonoBehaviour
+{
+    [SerializeField]
+    private RLog _logger;
+    private RHttp _http;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _http = new RHttp(_logger);
+        
+    }
+    void Start()
+    {
+        StartCoroutine(_http.Get("https://weather.tsukumijima.net/api/forecast/city/400040"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
